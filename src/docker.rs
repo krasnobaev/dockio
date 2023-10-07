b490138269f05c1b8262e015718478b46cae9c89
Aleksey Krasnobaev <a.krasnobaev@unicon.ru>
2023-10-07 05:07:39 +0300

    deserealize docker structure

---
+++ src/docker.rs
@@ -0,0 +1,82 @@
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Container {
+    #[serde(rename = "Command")]
+    pub command: String,
+    #[serde(rename = "CreatedAt")]
+    pub created_at: String,
+    #[serde(rename = "ID")]
+    pub id: String,
+    #[serde(rename = "Image")]
+    pub image: String,
+    #[serde(rename = "Labels")]
+    pub labels: String,
+    #[serde(rename = "LocalVolumes")]
+    pub local_volumes: String,
+    #[serde(rename = "Mounts")]
+    pub mounts: String,
+    #[serde(rename = "Names")]
+    pub names: String,
+    #[serde(rename = "Networks")]
+    pub networks: String,
+    #[serde(rename = "Ports")]
+    pub ports: String,
+    #[serde(rename = "RunningFor")]
+    pub running_for: String,
+    #[serde(rename = "Size")]
+    pub size: String,
+    #[serde(rename = "State")]
+    pub state: String,
+    #[serde(rename = "Status")]
+    pub status: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ContainerFront {
+    #[serde(rename = "Image")]
+    pub image: String,
+    #[serde(rename = "Names")]
+    pub names: String,
+    #[serde(rename = "Ports")]
+    pub ports: String,
+    #[serde(rename = "RunningFor")]
+    pub running_for: String,
+    #[serde(rename = "Size")]
+    pub size: String,
+    #[serde(rename = "State")]
+    pub state: String,
+    #[serde(rename = "Status")]
+    pub status: String,
+}
+
+impl From<&Container> for ContainerFront {
+    fn from(item: &Container) -> Self {
+        ContainerFront {
+            image: item.image.clone(),
+            names: item.names.clone(),
+            ports: item.ports.clone(),
+            running_for: item.running_for.clone(),
+            size: item.size.clone(),
+            state: item.state.clone(),
+            status: item.status.clone(),
+        }
+    }
+}
+
+impl Into<ContainerFront> for Container {
+    fn into(self) -> ContainerFront {
+        ContainerFront {
+            image: self.image.clone(),
+            names: self.names.clone(),
+            ports: self.ports.clone(),
+            running_for: self.running_for.clone(),
+            size: self.size.clone(),
+            state: self.state.clone(),
+            status: self.status.clone(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,69 @@
-use std::{env, io::Error, net::SocketAddr};
+use std::{env, io::Error, net::SocketAddr, sync::{Arc, Mutex}, collections::HashMap};
 
-use futures_util::{future, StreamExt, TryStreamExt};
+use futures_util::{future, StreamExt, TryStreamExt, pin_mut};
 use log::info;
-use tokio::net::{TcpListener, TcpStream};
+use tokio::{
+    net::{TcpListener, TcpStream}, time::{sleep, Duration}
+};
+
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use tokio_tungstenite::tungstenite::protocol::Message;
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+
+use std::process::Command;
+
+mod docker;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let _ = env_logger::try_init();
     let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
-
-    // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(&addr).await;
     let listener = try_socket.expect("Failed to bind");
     println!("Listening on: {}", addr);
 
+    let peer_map = PeerMap::new(Mutex::new(HashMap::new()));
+
     let docker_run_loop = async {
         loop {
-            let a = 0;
+            let output = Command::new("docker")
+                .args(["ps", "--format", "json"])
+                .output()
+                .expect("Failed to execute command");
+
+            let str = match std::str::from_utf8(&output.stdout) {
+                Ok(v) => v,
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+            let containers = str.lines().map(|row| {
+                let deserialized: docker::Container = serde_json::from_str(&row).unwrap();
+                deserialized
+            }).collect::<Vec<_>>();
+            let f_containers: Vec<docker::ContainerFront> = containers.iter().map(|c| {
+                c.into()
+            }).collect::<Vec<_>>();
+            let msg = Message::Text(format!("{f_containers:?}"));
+
+            // broadcast to all
+            let peers = peer_map.lock().unwrap();
+            let broadcast_recipients = peers
+                .iter()
+                .map(|(_, ws_sink)| ws_sink);
+            println!("transmit ({}) to all ({})", msg, broadcast_recipients.len());
+            for recipient in broadcast_recipients {
+                if let Err(e) = recipient.unbounded_send(msg.clone()) {
+                    println!("{}", e);
+                }
+            }
+
+            sleep(Duration::from_millis(3000)).await;
         }
     };
 
     let ws_run_loop = async {
         while let Ok((stream, addr)) = listener.accept().await {
-            tokio::spawn(accept_connection(stream, addr));
+            tokio::spawn(accept_connection(peer_map.clone(), stream, addr));
         }
     };
 
@@ -31,19 +72,41 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn accept_connection(stream: TcpStream, addr: SocketAddr) {
+async fn accept_connection(peer_map: PeerMap, stream: TcpStream, addr: SocketAddr) {
     let ws_stream = tokio_tungstenite::accept_async(stream)
         .await
         .expect("Error during the websocket handshake occurred");
+    println!("WebSocket connection established: {}", addr);
 
-    println!("New WebSocket connection: {}", addr);
+    // Insert the write part of this peer to the peer map.
+    let (tx, rx) = unbounded();
+    peer_map.lock().unwrap().insert(addr, tx);
 
-    let (write, read) = ws_stream.split();
-    // We should not forward messages other than text or binary.
-    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-        .forward(write)
-        .await
-        .expect("Failed to forward messages");
+    let (outgoing, incoming) = ws_stream.split();
+
+    let broadcast_incoming = incoming.try_for_each(|msg| {
+        println!("Received a message from {}: {}", addr, msg.to_text().unwrap());
+        let msg = Message::Text(format!("{msg} from {}", &addr).to_owned());
+        let peers = peer_map.lock().unwrap();
+
+        // We want to broadcast the message to everyone except ourselves.
+        let broadcast_recipients =
+            peers
+                .iter()
+                .map(|(_, ws_sink)| ws_sink);
+
+        for recp in broadcast_recipients {
+            recp.unbounded_send(msg.clone()).unwrap();
+        }
+
+        future::ok(())
+    });
+
+    let receive_from_others = rx.map(Ok).forward(outgoing);
+
+    pin_mut!(broadcast_incoming, receive_from_others);
+    future::select(broadcast_incoming, receive_from_others).await;
 
     println!("{} disconnected", &addr);
+    peer_map.lock().unwrap().remove(&addr);
 }
