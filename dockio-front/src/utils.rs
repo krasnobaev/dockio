670e6052b24a0cab6b7237a0cd19cfb3c4cbfa27
Aleksey Krasnobaev <a.krasnobaev@unicon.ru>
2023-10-14 11:47:05 +0300

    feat(front parse_mxfile_content): skip decompression if mxgraph is not compressed

--- dockio-front/src/utils.rs
+++ dockio-front/src/utils.rs
@@ -1,35 +1,52 @@
-use gloo::console::log;
+use gloo::console::{debug, log, info};
 
-use base64::{Engine as _, engine::general_purpose};
+use base64::{engine::general_purpose, Engine as _};
 use flate2::read::DeflateDecoder;
-use std::io::prelude::*;
 use js_sys::decode_uri_component;
-use web_sys::{DomParser, SupportedType, Element};
+use std::io::prelude::*;
+use web_sys::{DomParser, Element, SupportedType};
 
 use crate::model;
 
-pub fn parse_mxfile_content (bytes: Vec<u8>) -> (Element, crate::model::Nodes) {
+pub fn parse_mxfile_content(bytes: Vec<u8>) -> Option<(Element, crate::model::Nodes)> {
     let dia = String::from_utf8(bytes).unwrap();
-    // log!(format!("diagram message {:?}", dia));
+    debug!(format!("diagram message {:?}", dia.replace("\n", "")));
 
-    // embed svg into body
+    // build svg DOM Object
     let parser = DomParser::new().unwrap();
-    let svg_doc = parser.parse_from_string(&dia, SupportedType::TextHtml).unwrap();
+    let svg_doc = parser
+        .parse_from_string(&dia, SupportedType::TextHtml)
+        .unwrap();
     let svg_body = svg_doc.body().unwrap().children().item(0).unwrap();
 
-    // extract mxfile
+    // extract mxfile from SVG tag 'content' attribute
     let mxfile_tag = svg_body.get_attribute(&"content").unwrap();
-    let content = mxfile_tag.split_at(76).1.split_at(mxfile_tag.len() - 76 - 19).0;
-    // log!(format!("content {:?}", content));
+    debug!(format!("mxfile_tag {:?}", mxfile_tag));
+    let text = if mxfile_tag.contains("mxGraphModel") {
+        info!("mxfile tag is not compressed, skipping decompression and decoding");
+        mxfile_tag
+    } else {
+        let mxfile_tag = parser
+            .parse_from_string(&mxfile_tag, SupportedType::TextHtml)
+            .unwrap();
+        let mx_data = mxfile_tag.body().unwrap().children().item(0).unwrap().text_content().unwrap();
+        debug!(format!("mx_data {:?}", mx_data));
 
     // decode mxfile contents into xml object
-    let bytes = general_purpose::STANDARD.decode(content).unwrap();
+        let bytes = general_purpose::STANDARD.decode(mx_data).unwrap();
     let mut decoder = DeflateDecoder::new(bytes.as_slice());
     let mut text = String::new();
     decoder.read_to_string(&mut text).unwrap();
+        text
+    };
+
     let text = decode_uri_component(&text).unwrap().as_string().unwrap();
-    // log!(format!("content {:?}", text));
-    let xml_doc = parser.parse_from_string(&text, SupportedType::TextHtml).unwrap();
+    debug!(format!("content {:?}", text));
+    let xml_doc = parser
+        .parse_from_string(&text, SupportedType::TextHtml)
+        .unwrap();
+
+    debug!(format!("xml_doc {:?}", xml_doc));
 
     // map mx objects data into json
     let mx = xml_doc.get_elements_by_tag_name("object");
