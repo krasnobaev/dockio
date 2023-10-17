c39822dd06c48a8ff7ddef207e0a33a326106ac9
Aleksey Krasnobaev <a.krasnobaev@unicon.ru>
2023-10-17 20:14:21 +0300

    build: simple deployment shell script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+deploy() {
+  echo "Deploying... to ${TARGET_SERVER} as ${SERVER_USER}"
+
+  cargo build --target x86_64-unknown-linux-musl --release
+  cd dockio-front && trunk build --release && cd ..
+  wscat -n -L --connect "ws://${TARGET_SERVER}:${DOCKIO_WS_PORT}" -x 'Terminate'
+  scp target/x86_64-unknown-linux-musl/release/dockio "${SERVER_USER}@${TARGET_SERVER}:/opt/dockio/dockio"
+  scp -r dockio-front/dist/* "${SERVER_USER}@${TARGET_SERVER}:/opt/dockio/"
+
+  echo "done"
+}
+
+main() {
+  set -o allexport
+  # shellcheck source=/dev/null
+  source .env
+  set +o allexport
+
+  case "$1" in
+    "deploy")
+      deploy
+      ;;
+    *)
+      echo "Usage: $0 {deploy}"
+      exit 1
+      ;;
+  esac
+
+}
+
+main "$@"
