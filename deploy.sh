0ed21de514d73231150534695f6c3b9946c66a9a
Aleksey Krasnobaev <a.krasnobaev@unicon.ru>
2023-10-18 23:53:29 +0300

    build: deploy dev version by default

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 deploy() {
+  local release_flag="$1"
   echo "Deploying... to ${TARGET_SERVER} as ${SERVER_USER}"
   set -e
 
-  cargo build --target x86_64-unknown-linux-musl --release
-  cd dockio-front && trunk build --release && cd ..
+  cargo build --target x86_64-unknown-linux-musl ${release_flag}
+  cd dockio-front
+  trunk build ${release_flag}
+  cd ..
   wscat -n -L --connect "ws://${TARGET_SERVER}:${DOCKIO_WS_PORT}" -x 'Terminate'
   scp target/x86_64-unknown-linux-musl/release/dockio "${SERVER_USER}@${TARGET_SERVER}:/opt/dockio/dockio"
   scp -r dockio-front/dist/* "${SERVER_USER}@${TARGET_SERVER}:/opt/dockio/"
@@ -22,6 +25,9 @@ main() {
   case "$1" in
     "deploy")
       deploy
+      ;;
+    "deploy-release")
+      deploy --release
       ;;
     *)
       echo "Usage: $0 {deploy}"
